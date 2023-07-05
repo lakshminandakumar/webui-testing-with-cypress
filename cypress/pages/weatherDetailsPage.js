8f491a846097772f48f3f2abea70c1e43167498d
Lakshmi Nandakumar <lakshmi.nandakumar@drvnsolutions.com>
2023-07-05 17:12:15 +0100

    started adding implementation for weather page

--- cypress/pages/weatherDetailsPage.js
+++ cypress/pages/weatherDetailsPage.js
@@ -1,4 +1,16 @@
 class weatherDetailsPage{
 
+    elements = {
+        weatherIcon: () => cy.get('[data-cy="weather-icon"]'),
+        temperature: () => cy.get('[data-cy="temp"]'),
+        city: () => cy.get('[data-cy="city"]'),
+        humidityIcon: () => cy.get('[data-cy="humidity-icon"]'),
+        humidityValue: () => cy.get('[data-cy="humidity-value"]'),
+        humidityText: () => cy.get('[data-cy="humidity"]'),
+        windIcon: () => cy.get('[data-cy="wind-icon"]'),
+        windSpeedValue: () => cy.get('[data-cy="wind-speed-value"]'),
+        windSpeedText: () => cy.get('[data-cy="wind-speed"]')
+    }
+
 }
 module.exports = new weatherDetailsPage();
